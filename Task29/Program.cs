26f54d906d493e462414d5dbf91909fccb0a765d
Nikita <sorokovsky.ni@yandex.ru>
2022-12-06 21:54:54 +0300

    Done Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new  int[8];
+
+for (int  i = 0; i < n; i++)
+{
+    array[i] = new  Random().Next(0 , 100);
+            Console.Write(array[i] + "; ");
+            }
+Console.WriteLine();
